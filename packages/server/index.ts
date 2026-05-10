4606474efe64941e933fbaf9499357da94700e5a
saad <saadarif.lgu@gmail.com>
2026-05-10 12:02:21 +0500

    managed api key

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,10 +1,13 @@
 import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express()
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send("hello world");
 });
 
 app.listen(port, () => {
